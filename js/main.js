b2245e64abe7e4d7b7e91108ae1a9d28579b6691
Keenan Karich <kkarich@yahoo.com>
2016-04-15 03:21:18 +0000

    Updated '/js/main.js' via CloudCannon

--- js/main.js
+++ js/main.js
@@ -91,6 +91,7 @@ jQuery(function($) {
 	// Window Resize
 	$(window).resize(function() {
 		$('header').height($(window).height());
+		$('.header-overlay').height($(window).height());
 	});
 
 	// Pricing Box Click Event
